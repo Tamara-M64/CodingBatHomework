21c957a71e400af0a53f95ccb3bac10ae0af01e1
Tamara <tamara_margova@abv.bg>
2026-07-18 14:18:47 +0300

    Add icyHot solution

---
+++ src/main/java/com/demoqa/IcyHot.java
@@ -0,0 +1,14 @@
+package com.demoqa;
+
+public class IcyHot {
+    public boolean icyHot(int temp1, int temp2){
+        if((temp1<0 && temp2>100)|| (temp1>100&& temp2<0)){
+            return true;
+
+        }else{
+            return false;
+        }
+
+    }
+    }
+
