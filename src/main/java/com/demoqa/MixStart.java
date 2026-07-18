a19c268d597f932aa9131df240c0aab07f7a09c5
Tamara <tamara_margova@abv.bg>
2026-07-18 15:14:04 +0300

    Add mixStart solution

---
+++ src/main/java/com/demoqa/MixStart.java
@@ -0,0 +1,15 @@
+package com.demoqa;
+
+public class MixStart {
+    public boolean mixStart(String str){
+        if (str.length() <3){
+            return false;
+        }
+        String two = str.substring(1,3);
+        if (two.equals("ix")){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
