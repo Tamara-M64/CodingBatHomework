45eb6f5a96c4ee3936cc1780e0d2e416b750fab6
Tamara <tamara_margova@abv.bg>
2026-07-19 22:50:39 +0300

    Add maxEnd3 changed

--- src/main/java/com/demoqa/array1/MaxEnd3.java
+++ src/main/java/com/demoqa/array1/MaxEnd3.java
@@ -3,5 +3,6 @@ package com.demoqa.array1;
 public class MaxEnd3 {
     public int[] maxEnd3(int[] nums) {
         int max = Math.max(nums[0], nums[2]);
-        return new int[] {max, max, max};
+        return new int[]{max, max, max};
+    }
 }
