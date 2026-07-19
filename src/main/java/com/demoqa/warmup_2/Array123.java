1c8614660fffa5a4c9592d43a866a28f80a98659
Tamara <tamara_margova@abv.bg>
2026-07-19 11:33:34 +0300

    Add array123 solution

---
+++ src/main/java/com/demoqa/warmup_2/Array123.java
@@ -0,0 +1,13 @@
+package com.demoqa.warmup_2;
+
+public class Array123 {
+    public boolean array123(int[] nums){
+        for (int i = 0; i < nums.length-2; i++) {
+            if (nums[i] == 1 && nums[i] == 2 && nums[i] == 3){
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
