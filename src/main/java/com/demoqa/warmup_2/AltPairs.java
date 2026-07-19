c010317bf4166961c45831acf22f997e8bfc426d
Tamara <tamara_margova@abv.bg>
2026-07-19 14:03:24 +0300

    Add altParis solution

---
+++ src/main/java/com/demoqa/warmup_2/AltPairs.java
@@ -0,0 +1,16 @@
+package com.demoqa.warmup_2;
+
+public class AltPairs {
+    public String altPairs(String str) {
+        String result ="";
+        for(int i=0; i<str.length(); i+=4){
+            int end = i+2;
+            if(end > str.length()){
+                end = str.length();
+            }
+            result = result + str.substring(i,end);
+        }
+        return result;
+    }
+
+}
