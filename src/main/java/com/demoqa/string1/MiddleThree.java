6b9bb21bd29206409313d4e6a74fd837dae31e43
Tamara <tamara_margova@abv.bg>
2026-07-19 17:16:54 +0300

    Add middleThree solution

---
+++ src/main/java/com/demoqa/string1/MiddleThree.java
@@ -0,0 +1,8 @@
+package com.demoqa.string1;
+
+public class MiddleThree {
+    public String middleThree(String str) {
+        int mid = str.length() / 2;
+        return str.substring(mid - 1, mid + 2);
+    }
+}
