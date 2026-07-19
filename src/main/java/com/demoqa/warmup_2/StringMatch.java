87d5126d9436832867c7163d9c8184adc80fc9d1
Tamara <tamara_margova@abv.bg>
2026-07-19 12:40:56 +0300

    Add StringMatch.java

--- src/main/java/com/demoqa/warmup_2/StringMatch.java
+++ src/main/java/com/demoqa/warmup_2/StringMatch.java
@@ -1,7 +1,7 @@
 package com.demoqa.warmup_2;
 
 public class StringMatch {
-    public int public int stringMatch(String a, String b) {
+    public int stringMatch(String a, String b) {
        int len = Math.min(a.length(),b.length());
        int count =0;
         for (int i = 0; i < len-1; i++) {
